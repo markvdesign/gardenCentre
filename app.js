97de6a3d2da6990040e1f01ee2c695edf4c13356
markvdesign <markvdesign@gmail.com>
2017-02-27 15:40:44 +1000

    Added logging and GUID generation libraries.

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ const express    = require('express'); // Load express
 const app        = express();
 const mongoose   = require('mongoose'); // To interact with mongo
 const bodyParser = require('body-parser'); // body-parser will handles our API calls
+const logger     = require('winston'); // Use logger.info("info message"), logger.warn("warn msg") logger.error("error msg") or logger.log('info', "info msg")
+const uuid       = require('uuid/v1'); // Use uuid() to generate a timestamped GUID
+
 const config     = require('./gcConfig'); // Hide away our keys
 const crypto     = require('crypto');
 const encryption = require('./encryption');
 const jwt        = require('jsonwebtoken'); // used to create, sign, and verify tokens
+
+const sensor     = require('./waterSensor');
 
 // Data Models
 const SoilReading = require('./models/soilReading');
@@ -25,7 +30,6 @@ app.set('', config.tokenSecret);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 
 // ROUTES FOR OUR API
 // =============================================================================
@@ -185,17 +189,15 @@ apiRoutes.post('/user', (req, res) => {
 // *****************
 // IoT
 // *****************
-app.get('/api/getsoilreadings', function(req, res){
-    SoilReading.getReadings(function(err, readings){
-        if(err){
-            throw err;
-        }
-        res.json(readings);
-    });
+app.get('/api/getsoilreading', function(req, res){
+    res.json(SoilReading.getReading());
 });
 
 
 // START THE SERVER
 // =============================================================================
 app.listen(port); // Listen on the port number we set above
-console.log(`The magic happens on port ${port}`);
+logger.info(`The magic happens on port ${port}`);
+
+// Start our Water sensor
+sensor.start();
